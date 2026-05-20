f93a662220cb7742812e360085293350de4d9ccc
chenliuyun <chenliuyun@onero.com>
2026-05-20 22:57:31 +0800

    refactor(auth): extract shared getFreePort and escapeHtml into utils.ts

---
+++ src/auth/utils.ts
@@ -0,0 +1,24 @@
+import * as net from 'node:net';
+
+export async function getFreePort(): Promise<number> {
+  return new Promise((resolve, reject) => {
+    const srv = net.createServer();
+    srv.listen(0, '127.0.0.1', () => {
+      const addr = srv.address() as net.AddressInfo | null;
+      srv.close(() => {
+        if (!addr) { reject(new Error('Could not allocate port')); return; }
+        resolve(addr.port);
+      });
+    });
+    srv.on('error', reject);
+  });
+}
+
+export function escapeHtml(s: string): string {
+  return s
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
